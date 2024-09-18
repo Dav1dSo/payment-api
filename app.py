84154c8962ba27e42abbf3d511f9c34f5fe981cd
David Soares <davidsoares.dev@gmail.com>
2024-09-18 18:51:26 -0300

    feat: implement payment confirmation page
    Adds a dedicated page for confirming PIX payments, displaying the QR code, payment details, and expiration timer. This enhances the user experience by providing a clear and informative confirmation flow.
    
    This commit also introduces the necessary HTML templates and styles for the new confirmation page, ensuring a visually appealing and user-friendly design.

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, send_file
+from flask import Flask, request, send_file, render_template
 from factory import db
 from flask_migrate import Migrate
 from models import Payments
@@ -7,11 +7,14 @@ from services.payments import create_payment_pix
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "your-secret-key"
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://user_api:password_123@localhost:5432/payments'
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.config["SQLALCHEMY_DATABASE_URI"] = (
+    "postgresql://user_api:password_123@localhost:5432/payments"
+)
 
 db.init_app(app)
 migrate = Migrate(app, db)
+
 
 @app.route("/")
 def home():
@@ -26,7 +29,8 @@ def payment_pix_create():
         return create_payment_pix(data)
     except Exception as err:
         logging.error(f"{type(err)} - {err}")
-        return {'error': 'Ocorreu um error ao criar pagamento pix!'}, 500
+        return {"error": "Ocorreu um error ao criar pagamento pix!"}, 500
+
 
 @app.route("/payments/pix/confirmation", methods=["POST"])
 def pix_confirmation():
@@ -37,15 +41,23 @@ def pix_confirmation():
 @app.route("/payments/pix/confirmation/<int:payment_id>", methods=["GET"])
 def get_payment_pix(payment_id):
     """Retorna pagamento por id"""
-    return "Retorna pagamento por id!"
+
+    payment = db.session.query(Payments).filter(Payments.id == payment_id).first()
+
+    return render_template(
+        "payment.html",
+        payment_id=payment.id,
+        valor=payment.value,
+        qr_code=payment.qr_code,
+        host="localhost:5000"
+    )
 
 
 @app.route("/payments/pix/qr_code/<filename>", methods=["GET"])
 def get_qr_code(filename):
-    """ Retorna o qr_code pix """
-    return send_file(f"static/img/{filename}.png", mimetype="image/png") 
+    """Retorna o qr_code pix"""
+    return send_file(f"static/img/{filename}.png", mimetype="image/png")
 
 
 if __name__ == "__main__":
     app.run(debug=True)
-    
--- services/payments/Bank/Pix/create_payment_bank.py
+++ services/payments/Bank/Pix/create_payment_bank.py
@@ -9,13 +9,13 @@ class Pix:
     def create_payment(self):
         bank_payment_id = uuid.uuid4()
         
-        hash_payment = f"hash_payment_{bank_payment_id}"
+        qr_code_payment = f"qr_code_payment_{bank_payment_id}"
         
-        img = qrcode.make(hash_payment)
+        img = qrcode.make(qr_code_payment)
         
         img.save(f"static/img/qr_code_payment_{bank_payment_id}.png")
         
         return {
             "bank_payment_id": bank_payment_id,
-            "qr_code_path": hash_payment 
+            "qr_code_path": qr_code_payment 
         }
